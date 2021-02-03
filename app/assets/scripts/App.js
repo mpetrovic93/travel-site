9b67f0c4ff855c549cd564160bf47627dbce5516
MarkoPosao <petrovic.j.marko@gmail.com>
2021-02-03 14:35:00 +0100

    Completed Mobile navigation

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,31 @@
 import '../styles/styles.css'
+//import Person from './modules/Person'
+import MobileMenu from './modules/MobileMenu'
+
+let mobilemenu = new MobileMenu();
 
 if(module.hot){
     module.hot.accept()
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* Lesson example code below this line */ 
 
+/*let john = new Person("John Doe", "blue");
+john.greet();*/
+
+/*let jane = new Person("Jane Smith", "green");
+jane.greet();*/
